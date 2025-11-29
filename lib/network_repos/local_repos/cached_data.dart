971c2273f5d7e5d50897ac036182ad211ccf4c2d
AhmedShaltout85 <eng.abozaher@gmail.com>
2025-11-29 09:42:03 +0200

    Last-Update: 2025-11-29

--- lib/network_repos/local_repos/cached_data.dart
+++ lib/network_repos/local_repos/cached_data.dart
@@ -35,4 +35,19 @@ class SharedPreferencesService {
   Future<void> clearPreferences() async {
     await _preferences?.clear();
   }
+
+  // Add more methods as needed for other preferences
+  Future<bool> saveData(String key, dynamic value) async {
+
+    if(value is String) return await _preferences?.setString(key, value) ?? false;
+    if(value is int) return await _preferences?.setInt(key, value) ?? false;
+    if(value is bool) return await _preferences?.setBool(key, value) ?? false;
+    if(value is double) return await _preferences?.setDouble(key, value) ?? false;
+    if(value is List<String>) return await _preferences?.setStringList(key, value) ?? false;
+    return await _preferences?.setDouble(key, value) ?? false;
+  }
+
+  dynamic getData(String key) {
+    return _preferences?.get(key);
+  }
 }
